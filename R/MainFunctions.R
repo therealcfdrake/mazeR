22161508a89c0a76a49add8abca5abcc4dc42217
cfdrake <corie.drake@gmail.com>
2023-09-03 14:10:22 -0400

    first complete version

--- R/MainFunctions.R
+++ R/MainFunctions.R
@@ -1,3 +1,4 @@
+#' @import methods
 
 setClass(
   "Maze",
@@ -21,9 +22,12 @@ setClass(
   )
 )
 
-
 setMethod("show", signature(object = "Maze"), function(object){
-  print(render_maze(object))
+  if(object@tile == "square"){
+    print(render_maze(object))
+  }else{
+    print(render_maze_tri(object))
+  }
 })
 
 
@@ -42,6 +46,8 @@ is.even <- function(x){
   x %% 2 == 0
 }
 
+#' @importFrom magrittr equals
+
 curler2 <-
   function(base_weights, curl, curl_weight){
     n_dir <- length(base_weights)
@@ -57,7 +63,7 @@ curler2 <-
 #' @param w_maze width of maze
 #' @param l_maze length of maze
 #' @param h_maze height (number of layers) of maze
-#' @param exit `c(w, l, h)` coordinates of exit
+#' @param exit coordinates of exit
 #' @param i_weight a positive weight given to random directions in the i direction
 #' @param j_weight a positive weight given to random directions in the j direction
 #' @param k_weight a positive weight given to random directions in the k direction
@@ -73,11 +79,12 @@ curler2 <-
 #' @param mask_dir a .bmp file to use as a mask
 #' @param ... other parameters
 #' @examples
-#' library(tidyverse)
 #' generate_maze()
 #' @export
+#' @importFrom stats rbinom
+#' @import utils
 #' @import tidyr
-#' @import magrittr
+#' @importFrom magrittr equals
 #' @import bmp
 #' @returns a Maze object
 
@@ -85,7 +92,7 @@ generate_maze <-
   function(w_maze = 10,
            l_maze = 10,
            h_maze = 1,
-           exit = c(w_maze, l_maze, h_maze),
+           exit = NULL,
            i_weight = 1,
            j_weight = 1,
            k_weight = 0.1,
@@ -95,7 +102,8 @@ generate_maze <-
            uncurl = FALSE,
            progress = FALSE,
            mask_dir = NULL,
-           seed = NULL, ...){
+           seed = NULL, 
+           ... = NULL){
 
   # function to check whether bmp mask allows move
   move_allowed <-
@@ -114,8 +122,9 @@ generate_maze <-
     bw_mask <-
       round(0^sqrt(
         ((tmp_bmp[, , 1] / 255)^2 +
-           (tmp_bmp[, , 2] / 255)^2 +
-           (tmp_bmp[, , 3] / 255)^2) / 3), 0) %>%
+         (tmp_bmp[, , 2] / 255)^2 +
+         (tmp_bmp[, , 3] / 255)^2) / 
+          3), 0) %>%
       .[nrow(.):1, ]
 
     w_maze <- nrow(bw_mask)
@@ -128,8 +137,9 @@ generate_maze <-
   if(!is.null(seed)){set.seed(seed)}
 
   # define exit, if not provided
-  if(is.null(exit)){exit <- c(sample(1:w_maze, 1), sample(1:l_maze, 1), sample(1:h_maze, 1))}
-
+  if(is.null(exit)){exit <- c(w_maze, l_maze, h_maze)}
+  if(length(exit) == 2){exit <- c(exit, 1)}
+  
   # initialize variables
   n_cells <- w_maze * l_maze * h_maze
   maze_array <-
@@ -167,7 +177,7 @@ generate_maze <-
     if(cell_counter == n_cells){break}
 
     ###########################
-    ###  RNG-direction map  ###
+    ###    Direction map    ###
     ###########################
     #   1     <->    +i       #
     # 5 4 2 3 <-> -j -k +j +k #
@@ -187,7 +197,6 @@ generate_maze <-
       if(j > 1L)    {if(maze_array[i, j - 1, k, 4] != 1L && move_allowed(bw_mask, i, j, 5)){move_probs[5] <- j_weight}}
       if(k > 1L)    {if(maze_array[i, j, k - 1, 4] != 1L){move_probs[4] <- k_weight}}
 
-      # if(!apply_curl){apply_curl <-  rbinom(1, 1, curl_prob) == 1}
 
       if(apply_curl && rbinom(1, 1, curl_prob) == 1){
         if(last_dir == 1L){
@@ -208,7 +217,6 @@ generate_maze <-
       if(sum(move_probs) == 0L){
         path <- head(path, -1)
         tmp <- tail(path, 1)[[1]]
-        # apply_curl <- FALSE
         last_dir <- 0L
         i <- tmp[1]
         j <- tmp[2]
@@ -261,9 +269,11 @@ generate_maze <-
 #' @param maze a Maze object
 #' @param show_solution a logical describing whether to plot the solution to the maze
 #' @examples
-#' library(tidyverse)
-#' generate_maze()
+#' a <- generate_maze()
+#' render_maze(a, show_solution = TRUE)
 #' @export
+#' @importFrom stats rbinom
+#' @import utils
 #' @import ggplot2
 #' @import dplyr
 #' @import stringr
@@ -384,54 +394,318 @@ render_maze <-
 
   # create ggplot of maze
   if(h_maze == 1){
-    plot_out <-
-      plotdata_walls %>%
-      ggplot(aes(x = x, y = y)) +
-      geom_path(aes(group = id), lineend = "round", size = 0) +
-      geom_path(data = plotdata_solution, aes(color = id), size = 1, lineend = "round", linejoin = "round") +
-      scale_color_gradient(low = "limegreen", high = "purple4") +
-      geom_point(data = plotdata_points, aes(shape = d), color = "black") +
-      scale_shape_manual(values = c("u" = 2, "d" = 6, "none" = 16)) +
-      scale_x_continuous(breaks = 1:l_maze) +
-      scale_y_continuous(breaks = 1:w_maze) +
-      coord_equal() +
-      theme(panel.background = element_rect(fill = NA),
-            axis.text = element_blank(),
-            axis.ticks = element_blank(),
-            axis.title = element_blank(),
-            legend.position = "none",
-            plot.margin = unit( c(0,0,0,0), "in" ),
-            axis.ticks.length = unit(0, "cm")) +
-      NULL} else { # alternate format for mazes with more than 1 floor
-        plot_out <-
-          plotdata_walls %>%
-          ggplot(aes(x = x, y = y)) +
-          # geom_path(aes(group = id), lineend = "round", linejoin = "round") +
-          geom_path(aes(group = id), lineend = "round", size = 0) +
-          geom_path(data = plotdata_solution, aes(group = id, color = factor(id, levels = 0:n_sol_path, ordered = TRUE)), size = 1, lineend = "round", linejoin = "round") +###
-          scale_color_manual(values = colorRampPalette(c("goldenrod1", "springgreen4", "purple4"))(n_sol_path)) + ###
-          geom_point(data = plotdata_points, aes(shape = d, fill = d), color = "black", alpha = 0.5) + ###
-          scale_shape_manual(values = c("u" = 24, "d" = 25, "none" = 21)) + ###
-          scale_fill_manual(values = c("u" = "deepskyblue", "d" = "deeppink", "none" = "green")) + ###
-          scale_x_continuous(breaks = 1:l_maze) +
-          scale_y_continuous(breaks = 1:w_maze) +
-          coord_equal() +
-          facet_rep_wrap(paste("Level", str_pad(z, width = nchar(h_maze), side = "left", pad = "0")) ~ ., repeat.tick.labels = TRUE) + ###
-          theme(panel.background = element_rect(fill = NA),
-                axis.text = element_text(color = "gray50"), ###
-                axis.text.x = element_text(angle = 90, hjust = 1.0, vjust = 0.5), ###
-                axis.ticks = element_blank(),
-                axis.title = element_blank(),
-                panel.grid.major = element_line(color = "gray95"), ###
-                legend.position = "none") +
-          NULL
-      }
+    plot_out <- maze_plot(plotdata_walls, plotdata_points, plotdata_solution, l_maze, w_maze, 1)
+      } else { # alternate format for mazes with more than 1 floor
+        plot_out <- maze_plot_3d(plotdata_walls, plotdata_points, plotdata_solution, l_maze, w_maze, h_maze, 1, n_sol_path)
+        }
 
   plot_out
 
 }
 
-maze_plot_square <- function(plotdata_walls, plotdata_points, plotdata_solution){
+#' Generate Maze Object on Triangular Grid
+#'
+#' @param w_maze width of maze
+#' @param l_maze length of maze
+#' @param exit `c(w, l)` coordinates of exit
+#' @param i_weight a positive weight given to random directions in the i direction
+#' @param j_weight a positive weight given to random directions in the j direction
+#' @param curl a real on the interval `[-1, 1]` with negative values favoring clockwise
+#' turns and positive values favoring counterclockwise turns. a curl of 0 favors continuing
+#' in a straight line.
+#' @param curl_weight a positive weight given to curl related
+#' @param curl_prob a numeric value describing the probability that curl is applied. Once applied, curl
+#'  will influence subsequent direction choices until the algorithm reaches a dead end.
+#' @param uncurl a logical which causes the direction of curl to alternate each time it resets
+#' @param progress a logical which triggers an update to print each time 1000 cells have been visited
+#' @param seed a real value which can be passed as a seed to reproduce a maze
+#' @param ... other parameters
+#' @examples
+#' generate_maze_tri()
+#' @export
+#' @importFrom stats rbinom
+#' @import utils
+#' @import tidyr
+#' @importFrom magrittr equals
+#' @import bmp
+#' @returns a Maze object
+
+generate_maze_tri <-
+  function(w_maze = 5,
+           l_maze = 10,
+           exit = NULL,
+           i_weight = 1,
+           j_weight = 1,
+           curl = 0,
+           curl_weight = 0,
+           curl_prob = 0,
+           uncurl = FALSE,
+           progress = FALSE,
+           seed = NULL, 
+           ... = NULL){
+    
+    # set seed, if provided
+    if(!is.null(seed)){set.seed(seed)}
+    
+    # define exit, if not provided
+    if(is.null(exit)){exit <- c(w_maze, l_maze)}
+    
+    # initialize variables
+    n_cells <- w_maze * l_maze
+    maze_array <-
+      array(c(rep(outer(1:w_maze, 1:l_maze - 1,"+") %% 2, 3),
+              rep(0L, n_cells)),
+            dim = c(w_maze, l_maze, 4))
+    available_set <- c(1L, 2L, 3L, 4L, 5L, 6L)
+    apply_curl <- FALSE
+    last_dir <- 0L
+    path <- list()
+    i <- 1
+    j <- 1
+    
+    cell_counter <- 0
+    
+    # find path through all cells
+    while(cell_counter < n_cells){
+      
+      # print update status every 10000 cells if `progress` is set
+      if(cell_counter %% 1000 == 0 & progress){print(cell_counter)}
+      
+      # list of coordinates from start to current position
+      path <- lappend(path, c(i, j))
+      
+      # save solution if you've reached the end
+      if(all(c(i, j) == exit)){solution <- path}
+      
+      cell_counter <- cell_counter + 1
+      
+      # mark current cell as "visited"
+      maze_array[i, j, 4] <- 1L
+      
+      # stop if all cells have been visited
+      if(cell_counter == n_cells){break}
+      
+      ###########################
+      ###    Direction map    ###
+      ###########################
+      
+      # initialize vector containing probabilities of moving in each direction
+      move_probs <- rep(0L, 6)
+      
+      # update probabilities
+      while(sum(move_probs) == 0L){
+        
+        if(i < w_maze){if(maze_array[i + 1, j, 4] != 1L && is.odd(i + j)){move_probs[1] <- i_weight}}
+        if(j < l_maze){if(maze_array[i, j + 1, 4] != 1L){move_probs[2] <- j_weight}}
+        if(i > 1L)    {if(maze_array[i - 1, j, 4] != 1L && is.even(i + j)){move_probs[6] <- i_weight}}
+        if(j > 1L)    {if(maze_array[i, j - 1, 4] != 1L){move_probs[5] <- j_weight}}
+        
+        
+        if(apply_curl && rbinom(1, 1, curl_prob) == 1){
+          if(last_dir == 1L){
+            move_probs[c(5, 2, 6)] <- curler2(move_probs[c(5, 2, 6)], curl, curl_weight)
+          }
+          if(last_dir == 2L && is.odd(i + j)){
+            move_probs[c(1, 2, 5)] <- curler2(move_probs[c(1, 2, 5)], curl, curl_weight)
+          }
+          if(last_dir == 2L && is.even(i + j)){
+            move_probs[c(6, 2, 5)] <- curler2(move_probs[c(6, 2, 5)], curl, curl_weight)
+          }
+          if(last_dir == 6L){
+            move_probs[c(2, 5, 1)] <- curler2(move_probs[c(2, 5, 1)], curl, curl_weight)
+          }
+          if(last_dir == 5L && is.odd(i + j)){
+            move_probs[c(6, 5, 2)] <- curler2(move_probs[c(6, 5, 2)], curl, curl_weight)
+          }
+          if(last_dir == 5L && is.even(i + j)){
+            move_probs[c(5, 1, 2)] <- curler2(move_probs[c(5, 1, 2)], curl, curl_weight)
+          }
+        }
+        
+        # backtrack if no paths forward are available
+        if(sum(move_probs) == 0L){
+          path <- head(path, -1)
+          tmp <- tail(path, 1)[[1]]
+          last_dir <- 0L
+          i <- tmp[1]
+          j <- tmp[2]
+          if(uncurl){curl <- -curl}
+        }
+        
+      }
+      
+      # choose direction of next move
+      next_dir <- sample(available_set, 1, prob = move_probs)
+      
+      i2 <- i
+      j2 <- j
+      
+      if(next_dir == 1L){i2 <- i + 1}
+      if(next_dir == 2L){j2 <- j + 1}
+      if(next_dir == 5L){j2 <- j - 1}
+      if(next_dir == 6L){i2 <- i - 1}
+      
+      if(next_dir == 1L && is.even(i + j)){maze_array[i, j, 1] <- 0L}
+      if(next_dir == 1L && is.odd(i + j)){maze_array[i + 1, j, 1] <- 0L}
+      if(next_dir == 6L && is.even(i + j)){maze_array[i, j, 1] <- 0L}
+      if(next_dir == 6L && is.odd(i + j)){maze_array[i - 1, j, 1] <- 0L}
+      if(next_dir == 2L && is.even(i + j)){maze_array[i, j, 2] <- 0L}
+      if(next_dir == 2L && is.odd(i + j)){maze_array[i, j + 1, 3] <- 0L}
+      if(next_dir == 5L && is.even(i + j)){maze_array[i, j, 3] <- 0L}
+      if(next_dir == 5L && is.odd(i + j)){maze_array[i, j - 1, 2] <- 0L}
+      
+      
+      i <- i2
+      j <- j2
+      last_dir <- next_dir
+      
+    }
+    
+    # create Maze S4 object
+    new("Maze",
+        width = w_maze,
+        length = l_maze,
+        height = 1,
+        array = maze_array,
+        exit = exit,
+        solution = solution,
+        tile = "triangle")
+  }
+
+#' Render a Triangular Grid Maze object
+#'
+#' @param maze a Maze object
+#' @param show_solution a logical describing whether to plot the solution to the maze
+#' @examples
+#' generate_maze_tri()
+#' @export
+#' @importFrom stats rbinom
+#' @import utils
+#' @import ggplot2
+#' @import dplyr
+#' @import stringr
+#' @returns ggplot of Maze object
+
+
+render_maze_tri <-
+  function(maze, show_solution = FALSE){
+    
+    w_maze <- maze@width
+    l_maze <- maze@length
+    h_maze <- maze@height
+    maze_array <- maze@array
+    exit <- maze@exit
+    solution <- maze@solution
+    
+    # initialize solution df
+    plotdata_solution <-
+      data.frame(y = c(1, 1), x = c(1, 1), id = c(0, 0))
+    
+    # build solution df
+    if(show_solution) {
+      plotdata_solution <-
+        solution %>%
+        unlist %>%
+        matrix(ncol = 2,
+               byrow = TRUE,
+               dimnames = list(NULL, c("y", "x"))
+        ) %>%
+        data.frame() %>%
+        mutate(across(c(x, y), ~(.x + lag(.x, default = 0)) / 2)) %>% 
+        mutate(id = row_number())
+    }
+    
+    # length of solution path
+    n_sol_path <-
+      pull(plotdata_solution) %>%
+      unique %>%
+      length
+    
+    # initialize maze walls list with borders
+    wallslist <-
+      list(data.frame(id = rep("edge", 2 * w_maze + 3),
+                      x = c(l_maze - is.even(l_maze), # bottom right
+                            0, # bottom left
+                            as.integer(is.odd(1:w_maze)), # zigzag left
+                            l_maze + as.integer(is.odd(l_maze + w_maze:0))), # zigzag right
+                      y = c(0.5, 
+                            0.5, 
+                            0.5 + 1:w_maze, 
+                            0.5 + w_maze:1,
+                            0.5)))
+    
+    ##### extract coordinates for horizontal walls
+    
+    for(i in 1:w_maze){
+      for(j in 1:l_maze){
+        if(maze_array[i, j, 1] == 1L){
+          wallslist <-
+            lappend(wallslist,
+                    data.frame(id = rep(paste0("i", str_pad(i, width = nchar(w_maze), side = "left", pad = "0"), str_pad(j, width = nchar(l_maze), side = "left", pad = "0")), 2),
+                               x = c(j - 1, j + 1),
+                               y = c(i - 0.5, i - 0.5))
+            )
+        }
+      }
+    }
+    
+    ##### extract coordinates for western sloped walls
+    
+    for(i in 1:w_maze){
+      for(j in 1:l_maze){
+        if(maze_array[i, j, 2] == 1L){
+          wallslist <-
+            lappend(wallslist,
+                    data.frame(id = rep(paste0("j", str_pad(i, width = nchar(w_maze), side = "left", pad = "0"), str_pad(j, width = nchar(l_maze), side = "left", pad = "0")), 2),
+                               x = c(j, j + 1),
+                               y = c(i + 0.5, i - 0.5))
+            )
+        }
+      }
+    }
+    
+    ##### extract coordinate for eastern sloped walls
+    
+    ladderlist <- list()
+    
+    for(i in 1:w_maze){
+      for(j in 1:l_maze){
+        if(maze_array[i, j, 3] == 1L){
+          wallslist <-
+            lappend(wallslist,
+                    data.frame(id = rep(paste0("k", str_pad(i, width = nchar(w_maze), side = "left", pad = "0"), str_pad(j, width = nchar(l_maze), side = "left", pad = "0")), 2),
+                               x = c(j, j - 1),
+                               y = c(i + 0.5, i - 0.5))
+            )
+        }
+      }
+    }
+    
+    # define start- and end-points of path
+    startenddata <-
+      data.frame(id = c("start", "end"),
+                 x = c(1, exit[2]),
+                 y = c(1, exit[1]),
+                 d = c("none", "none"))
+    
+    plotdata_walls <-
+      bind_rows(wallslist)
+    
+    plotdata_points <-
+      bind_rows(ladderlist) %>%
+      bind_rows(startenddata)
+    
+    # create ggplot of maze
+    
+    plot_out <- maze_plot(plotdata_walls, plotdata_points, plotdata_solution, l_maze, w_maze, ratio = sqrt(3))
+    
+    plot_out
+    
+  }
+
+#' @import utils
+
+maze_plot <- function(plotdata_walls, plotdata_points, plotdata_solution, l_maze, w_maze, ratio){
   plotdata_walls %>%
     ggplot(aes(x = x, y = y)) +
     geom_path(aes(group = id), lineend = "round", size = 0) +
@@ -441,7 +715,7 @@ maze_plot_square <- function(plotdata_walls, plotdata_points, plotdata_solution)
     scale_shape_manual(values = c("u" = 2, "d" = 6, "none" = 16)) +
     scale_x_continuous(breaks = 1:l_maze) +
     scale_y_continuous(breaks = 1:w_maze) +
-    coord_equal() +
+    coord_fixed(ratio = ratio) +
     theme(panel.background = element_rect(fill = NA),
           axis.text = element_blank(),
           axis.ticks = element_blank(),
@@ -451,8 +725,31 @@ maze_plot_square <- function(plotdata_walls, plotdata_points, plotdata_solution)
           axis.ticks.length = unit(0, "cm"))
 }
 
-maze_plot_3d <- function(){
-  
+#' @import utils
+#' @import dplyr
+#' @importFrom grDevices colorRampPalette
+
+maze_plot_3d <- function(plotdata_walls, plotdata_points, plotdata_solution, l_maze, w_maze, h_maze, ratio, n_sol_path){
+  plotdata_walls %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_path(aes(group = id), lineend = "round", size = 0) +
+    geom_path(data = plotdata_solution, aes(group = id, color = factor(id, levels = 0:n_sol_path, ordered = TRUE)), size = 1, lineend = "round", linejoin = "round") +###
+    scale_color_manual(values = colorRampPalette(c("goldenrod1", "springgreen4", "purple4"))(n_sol_path)) + ###
+    geom_point(data = plotdata_points, aes(shape = d, fill = d), color = "black", alpha = 0.5) + ###
+    scale_shape_manual(values = c("u" = 24, "d" = 25, "none" = 21)) + ###
+    scale_fill_manual(values = c("u" = "deepskyblue", "d" = "deeppink", "none" = "green")) + ###
+    scale_x_continuous(breaks = 1:l_maze) +
+    scale_y_continuous(breaks = 1:w_maze) +
+    coord_equal() +
+    facet_rep_wrap(paste("Level", str_pad(z, width = nchar(h_maze), side = "left", pad = "0")) ~ ., repeat.tick.labels = TRUE) + ###
+    theme(panel.background = element_rect(fill = NA),
+          axis.text = element_text(color = "gray50"), ###
+          axis.text.x = element_text(angle = 90, hjust = 1.0, vjust = 0.5), ###
+          axis.ticks = element_blank(),
+          axis.title = element_blank(),
+          panel.grid.major = element_line(color = "gray95"), ###
+          legend.position = "none") +
+    NULL
 }
 
 
